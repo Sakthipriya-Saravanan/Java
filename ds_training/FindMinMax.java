bf3b30974bcf6420cc421f8d3a7285a8168b4cb7
Sakthipriya S <154298187+Sakthipriya-Saravanan@users.noreply.github.com>
2025-03-21 06:13:28 +0530

    FindMinMax.java

---
+++ ds_training/FindMinMax.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class FindMinMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int min = arr[0], max = arr[0];
+
+        for (int i = 1; i < size; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Smallest element: " + min);
+        System.out.println("Largest element: " + max);
+
+        scanner.close();
+    }
+}
